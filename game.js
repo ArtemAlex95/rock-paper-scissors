045f008b679981ed9d39df54a37b0f6f11544d2c
ArtemAlex95 <79396150+ArtemAlex95@users.noreply.github.com>
2021-04-22 22:28:06 +0300

    Added first results

---
+++ game.js
@@ -0,0 +1,37 @@
+//первый играет компьютер, рандомный выбор фигуры
+function computerPlay() {
+    const figures = ['Rock', 'Paper', 'Scissors'];
+    computerChoise = Math.floor(Math.random()*figures.length);
+    if ( computerChoise == '0') {
+        return 'rock';
+    }
+    else if (computerChoise == '1') {
+        return 'paper';
+    }
+    else {
+        return 'scissors';
+    }
+
+}
+
+    //преобразовать return предыдущей функции в computerSelection
+    let computerSelection = computerPlay();
+
+    function playRound(playerSelection, computerSelection) {
+        if (playerSelection == computerSelection) {
+            return 'It\'s a tie!'
+        } else if (playerSelection == 'rock' && computerSelection == 'scissors' || playerSelection == 'paper' && computerSelection == 'rock' || playerSelection == 'scissors' && computerSelection == 'paper') {
+            return 'You won!'
+        } else {
+            return 'You lose! :('
+        }
+      }
+
+    let prePlayerSelection = prompt('What will you chose?');
+    let playerSelection = prePlayerSelection.toLowerCase();
+    console.log(playRound(playerSelection, computerSelection));
+//функция game() до 5 реузльтативных выборов (попытаться сделать цикл)
+
+function game() {
+
+}
