03f517114a2ef6ada016b2311e696a305d38521f
ArtemAlex95 <79396150+ArtemAlex95@users.noreply.github.com>
2021-07-03 16:32:23 +0300

    fixed updateScore()

--- game.js
+++ game.js
@@ -1,17 +1,13 @@
-<<<<<<< HEAD
 let playerScore = 0;
 let computerScore = 0;
 let playerSelection;
 let computerSelection;
 
-=======
->>>>>>> 0cbff2a6eaae1911f3c84ebb86a45283e02fe424
 const figures = {
     ROCK: 'rock',
     SCISSORS: 'scissors',
     PAPER: 'paper'
 }
-<<<<<<< HEAD
 
 function beatFigure (figure) {
     switch (figure) {
@@ -85,68 +81,3 @@ function gameOver() {
         return 'scissors';
     }
 };
-
-=======
-
-function beatFigure (figure) {
-    switch (figure) {
-        case figures.ROCK:
-            return figures.SCISSORS;
-            break;
-        case figures.PAPER:
-            return figures.ROCK;
-            break;
-        case figures.SCISSORS:
-            return figures.PAPER;
-            break;
-    }
-}
-
-
-
-    function game() {
-
-    let computerScore = 0;
-    let playerScore = 0;
-
-    for (i = 0; computerScore < 3 && playerScore <3; i++) {
-
-        function computerPlay() {
-            const randomFigures = [1, 2, 3]
-            computerChoise = Math.floor(Math.random()*randomFigures.length);
-            if ( computerChoise == '0') {
-                return 'rock';
-            }
-            else if (computerChoise == '1') {
-                return 'paper';
-            }
-            else {
-                return 'scissors';
-            }
-        
-        }
-
-        let computerSelection = computerPlay();
-
-         let playerSelection = prompt('What will you choose?', 'Rock? Paper? Scissors?').toLowerCase();
-            console.log(playRound(playerSelection, computerSelection));
-
-        function playRound(playerSelection, computerSelection) {
-        if (playerSelection == computerSelection) {
-            return 'It\'s a tie!'
-        } else if (beatFigure(playerSelection) == computerSelection) {
-            playerScore = ++playerScore;
-            console.log(playerScore);
-            return 'You won!'
-        } else {
-            computerScore = ++computerScore;
-            console.log(computerScore);
-            return 'You lose! :('
-        }
-      }
-    }
-
-        
-      
-};
->>>>>>> 0cbff2a6eaae1911f3c84ebb86a45283e02fe424
